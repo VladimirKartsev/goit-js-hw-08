b3af8fc937552da24b55fcd0a021de6d26a3c904
Vladimir <kartcev2204@gmail.com>
2023-07-09 16:58:00 +0300

    9.07.23-16-49

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Vimeo from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const localKey = 'videoplayer-current-time';
+const iframe = document.querySelector('#iframe');
+const player = new Vimeo(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  const currentTime = JSON.stringify(data);
+  localStorage.setItem(localKey, currentTime);
+}
+const timeupdate = () => {
+  const currentTime = localStorage.getItem(localKey);
+  if (currentTime) {
+    const timeJson = JSON.parse(currentTime);
+    const { second } = timeJson;
+    return second;
+  }
+};
+player
+  .setCurrentTime(timeupdate())
+  .then(function (seconds) {})
+
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
